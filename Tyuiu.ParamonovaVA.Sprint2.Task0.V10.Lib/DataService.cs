506e9efb9922f2e63a758687f6c7712802dfbcb8
Tyuiu.ParamonovaVA@outlook.com <paramonova-05@mail.ru>
2024-12-10 01:20:28 +0500

    Add project files.

---
+++ Tyuiu.ParamonovaVA.Sprint2.Task0.V10.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.ParamonovaVA.Sprint2.Task0.V10.Lib
+{
+    public class DataService : ISprint2Task0V10
+    {
+        public bool[] GetCompareOperations(int a, int b)
+        {
+            bool[] result = new bool[6];
+            result[0] = a == b;
+            result[1] = a != b;
+            result[2] = a < b;
+            result[3] = a > b;
+            result[4] = a <= b;
+            result[5] = a >= b;
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.ParamonovaVA.Sprint2.Task0.V10/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.ParamonovaVA.Sprint2.Task0.V10.Lib;
+
+namespace Tyuiu.ParamonovaVA.Sprint2.Task0.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Парамонова В.А. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнила: Парамонова Валерия Алексеевна | РППб-24-2                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=)         *");
+            Console.WriteLine("* и арифметических выражений, которая вернет логическую последовательность*");
+            Console.WriteLine("*(массив): (False, True, False, True, False, True)                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* x = 1305, y = 475                                                       *");
+            int x = 1305;
+            int y = 475;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
