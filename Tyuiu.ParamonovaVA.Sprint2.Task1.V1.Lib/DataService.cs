c0fbaa899079cfc00f3f62168fb9ef713cb2e709
Tyuiu.ParamonovaVA@outlook.com <paramonova-05@mail.ru>
2024-12-10 02:35:40 +0500

    Task1

---
+++ Tyuiu.ParamonovaVA.Sprint2.Task1.V1.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ParamonovaVA.Sprint2.Task1.V1.Lib
+{
+    public class DataService : ISprint2Task1V1
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = (a > b) | (c < d); // True
+            res[1] = (a < b) & (c > d); // False
+            res[2] = (a + 2 < b) || (c < d); // True
+            res[3] = (a > b) && (c + 3 < d); // False 
+            res[4] = !(res![1]); // True
+            res[5] = (a + 90 > b) ^ (c < d); // False
+
+            //int a = 119; int b = 196; int c = 134; int d = 327;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ParamonovaVA.Sprint2.Task1.V1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ParamonovaVA.Sprint2.Task1.V1.Lib;
+
+namespace Tyuiu.ParamonovaVA.Sprint2.Task1.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int a = 119;
+            int b = 196;
+            int c = 134;
+            int d = 327;
+
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("Исходные данные");
+            Console.WriteLine("********************************************************************");
+
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ");
+            Console.WriteLine("********************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
